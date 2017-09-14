30343010d7a1dc328e0551de545fba0aa0ec012b
capntechbeard <capntechbeard@gmail.com>
2017-09-14 16:00:09 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function heartBeat (){
+  heartBeat2();
+  setTimeout(heartBeat1,150)
+  setTimeout(heartBeat2,300)
+  setTimeout(heartBeat1,450)
+  setTimeout(heartBeat2,1150)
+  setTimeout(heartBeat1,1300)
+  setTimeout(heartBeat2,1450)
+  setTimeout(heartBeat1,1600)
+  setTimeout(heartBeat,2300)
+}
+
+
+function heartBeat2 (x,y){
+    var x = document.querySelector("heart").style.fontSize = "7.5em";
+    var y = document.querySelector("heart").style.color = "#ff5d7a";
+    console.log("this works");
+  }
+
+function heartBeat1 (x,y){
+  var x = document.querySelector("heart").style.fontSize = "7em";
+  var y = document.querySelector("heart").style.color = "#f994a6";
+  console.log("this works");
+  }
